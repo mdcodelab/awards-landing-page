27e74ddf9611e69d54f29a09c946a77f57658954
miha11-hash <d_mihaela@msn.com>
2025-01-17 19:39:47 +0200

    define colors in tailwind.config

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Hone() {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awards!</h1>
+      <h1 className="text-5xl text-violet-300 font-bold">Welcome to Awards!</h1>
     </main>
   )
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,6 +14,23 @@ export default {
         robertMedium: ["robert-medium", "sans-serif"],
         robertRegular: ["robert-regular", "sans-serif"],
       },
+
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#dfdff2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724ff",
+        },
+        yellow: {
+          100: "#8e983f",
+          300: "#edff66",
+        },
+      }
     },
   },
   plugins: [],
