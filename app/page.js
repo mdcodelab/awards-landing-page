92bbf785502c3c36e1b2b3ae02662c1137bfb540
miha11-hash <d_mihaela@msn.com>
2025-02-10 11:09:01 +0200

    restructured files

--- app/page.js
+++ app/page.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import Hero from './(components)/Hero';
 
-function Hone() {
+function Home() {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Hero></Hero>
-    </main>
-  )
+    <>
+      <nav>
+        <div className="logo-container">
+          <h1>Creatives</h1>
+        </div>
+        <ul>
+          <li><a href="#">MAIN FEATURES</a></li>
+          <li><a href="#">PRODUCTS</a></li>
+          <li><a href="#">SHOP</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </nav>
+
+      <section className="sidebar"></section>
+
+      <section className="content-container">
+        <img src="/img/about.webp" alt="contact" className="main-img" />
+        <div className="content">
+          <h1 className="main-title">Creatives</h1>
+          <p className="main-info">
+            Welcome to the real world!
+            <br />
+            Explore our Gallery
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Hone
+export default Home;
