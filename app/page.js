f5b537b5e1055bd9409a6cefc7aec91c4a1a86b0
miha11-hash <d_mihaela@msn.com>
2025-02-10 19:41:49 +0200

    gsap acordeon

--- app/page.js
+++ app/page.js
@@ -1,54 +1,76 @@
 "use client";
 import React from 'react';
-import gsap from 'gsap';
 import { useEffect } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap-trial/ScrollTrigger';
+import ScrollToPlugin from 'gsap-trial/ScrollToPlugin';
 
-function Home() {
+function page() {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+console.clear();
 
 useEffect(()=> {
-let tl=gsap.timeline({
-  defaults: {
+  const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.accordions',
+        pin: true,
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        ease: 'linear',
+      }
+  })
+  
+  tl.to('.accordion .text', {
+    height: 0,
+    paddingBottom: 0,
     opacity: 0,
-    ese: "linear",
-    duration: 1
-  }
+    stagger: .5,
+  })
+  tl.to('.accordion', {
+    marginBottom: -15,
+    stagger: .5,
+  }, '<')
+  
 })
 
-tl.fromTo(".sidebar", {width: 0}, {width: "30%", opacity: 1});
-
-tl.fromTo(".main-img", {x: 0}, {opacity: 1});
-
-tl.fromTo(".content", {x: 100}, {x: 0, opacity: 1})
-}, [])
   return (
     <>
-      <nav>
-        <div className="logo-container">
-          <h1>Creatives</h1>
+      <div id="wrapper">
+  <div id="content">
+    <div className="spacer"></div>
+    <div className="accordions">
+      <div className="accordion">
+        <div className="title">All-screen design.</div>
+        <div className="text">
+          Lets you immerse yourself in whatever you’re reading, watching, or creating. The 10.9-inch Liquid Retina display features advanced technologies like True Tone, P3 wide color, and an antireflective coating.1
         </div>
-        <ul>
-          <li><a href="#">MAIN FEATURES</a></li>
-          <li><a href="#">PRODUCTS</a></li>
-          <li><a href="#">SHOP</a></li>
-          <li><a href="#">CONTACT</a></li>
-        </ul>
-      </nav>
+      </div>
 
-      <section className="sidebar"></section>
-
-      <section className="content-container">
-        <img src="/img/about.webp" alt="contact" className="main-img" />
-        <div className="content">
-          <h1 className="main-title">Creatives</h1>
-          <p className="main-info">
-            Welcome to the real world!
-            <br />
-            Explore our Gallery
-          </p>
+      <div className="accordion">
+        <div className="title"> Beauty all around.</div>
+        <div className="text">
+          The breakthrough M1 chip is now in Air. An 8-core CPU delivers up to 60 percent faster performance than the previous generation, making Air a creative and mobile gaming powerhouse. Multitask smoothly between powerful apps and play graphics-intensive games. And with M1, you can go even further with your creativity with apps like SketchUp.
         </div>
-      </section>
+      </div>
+
+      <div className="accordion">
+        <div className="title">Take Center Stage.</div>
+        <div className="text">
+          The 12MP Ultra Wide front camera enables Center Stage, making video calls more natural and content creation more fun. As you move around, the camera automatically pans to keep you centered in the shot. When others join or leave the frame, the view expands or zooms in.
+        </div>
+      </div>
+
+      <div className="accordion">
+        <div className="title">Pretty everywhere.</div>
+        <div className="text">Join superfast 5G wireless networks when you’re on the go. Download files, play multiplayer games, stream movies, check in with friends, and more.</div>
+      </div>
+    </div>
+    <div className="spacer"></div>
+  </div>
+</div>
     </>
-  );
+  )
 }
 
-export default Home;
+export default page
